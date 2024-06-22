4b163b45b69198468638d19a2746d1db962f2dc4
Praful Patekar <prafulpatekar23@gmail.com>
2024-06-23 01:36:58 +0530

    Adding Profile Model and Common Model

--- core_apps/profiles/admin.py
+++ core_apps/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ProfileModel
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "id", "user", "gender", "phone_number", "country", "city"]
+    list_display_links = ["pkid", "id", "user"]
+    list_filter = ["id", "pkid"]
+
+
+admin.site.register(ProfileModel, ProfileAdmin)
